8610f7b91af2b4e66ac45225f80a80f27ea1dde1
joel <joel@a91ba295-8668-4b9c-a9c6-a6c45bdfad03>
2010-01-09 20:06:53 +0000

    changed name of directory to src/
    git-svn-id: svn://svn.savannah.nongnu.org/haploid/haploid/trunk@730 a91ba295-8668-4b9c-a9c6-a6c45bdfad03

---
+++ src/haplibpriv.h
@@ -0,0 +1,42 @@
+#ifndef HAPLIBPRIV_H
+#define HAPLIBPRIV_H
+/*
+  haplibpriv.h: functions for numerical analysis of haploid genetics
+  Copyright 2009 Joel J. Adamson 
+  
+  $Id: func.h 497 2009-06-20 00:23:00Z joel $$
+
+  Joel J. Adamson	-- http://www.unc.edu/~adamsonj
+  University of North Carolina at Chapel Hill
+  CB #3280, Coker Hall
+  Chapel Hill, NC 27599-3280
+  <adamsonj@email.unc.edu>
+
+  This file is part of haploid
+
+  haploid is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  haploid is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with haploid.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/* includes: */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <math.h>
+#include <complex.h>
+#include <stdbool.h>
+#include <time.h>
+#include <fenv.h>
+#endif
